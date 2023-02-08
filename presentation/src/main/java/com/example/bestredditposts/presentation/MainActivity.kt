6d9dde1660e95b5ca529e76b638c5c5cf5fca24c
Dima <aashzir0@gmail.com>
2023-02-08 12:14:13 +0200

    Modules added

--- presentation/src/main/java/com/example/bestredditposts/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/bestredditposts/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bestredditposts
+package com.example.bestredditposts.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bestredditposts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
